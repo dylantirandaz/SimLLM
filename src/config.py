0fecc0aa6b7825780e7a81b749e62a984f4dc505
dylan tirandaz <93934418+dylantirandaz@users.noreply.github.com>
2025-04-30 12:12:41 -0500

    k

---
+++ src/config.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    #OpenAI
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    OPENAI_MODEL = "gpt-3.5-turbo"
+
+    #PATHS
+    DATA_DIR = "data/xsum"
+    MODEL_SAVE_DIR = "models/"
+
+    #BERT/RoBERTa
+    BART_CHECKPOINT = "facebook/bart-large-cnn"
+    ROBERTA_CHECKPOINT = "roberta-base"
+
+    #TRAINING
+    BATCH_SIZE = 64
+    LEARNING_RATE = 2e-5
+    NUM_EPOCHS = 10
